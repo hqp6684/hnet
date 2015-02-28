f6579c8c9c78be029264124db9c655c0810ea39a
Huy Pham <huypham1992@yahoo.com>
2015-02-27 19:35:48 -0500

    register with uesr name and pass

--- registration/views.py
+++ registration/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
-
+from registration.forms import UserCreationForm
 # Create your views here.
 
-def register(request):
-	return HttpResponse("hello, there")
+def register(request, template_name='registration/registration_form.html'):
+	form = UserCreationForm(request.POST)
+	if form.is_valid():
+		form.save()
+		return redirect('healthnet')
+	return render(request, template_name, {'form':form})
--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
-from users.models import Employee, Doctor, Nurse
+from users.models import Employee, Doctor, Nurse, Patient
 
 # Register your models here.
 
 
 admin.site.register(Employee)
+admin.site.register(Patient)
+admin.site.register(Nurse)

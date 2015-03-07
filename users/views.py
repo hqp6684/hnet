5667920e8217a283dfa5b4396dd2c3b7d2607955
Huy Pham <huypham1992@yahoo.com>
2015-03-07 09:48:18 -0500

    users index need to be a staff

--- users/views.py
+++ users/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+
 
 # Create your views here.
-
+@login_required(login_url='/login')
 def index(request):
+	if not request.user.is_staff:
+		return HttpResponseRedirect("/")
+	
 	users = User.objects.all()
 	context = {'users_list' : users}
 	
 	return render(request, 'users/index.html', context)
+
+	
